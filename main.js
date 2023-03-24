e42d2242f9645e304353e5208212c5b0e333fb4e
AG-code32 <i.gerson@hotmail.com>
2023-03-23 23:56:23 -0500

    footer created, some functions fixed, final

--- main.js
+++ main.js
@@ -8,11 +8,42 @@ openNavBtn.addEventListener('click', () => {
     closeNavBtn.style.display = 'inline-block';
 })
 
-closeNavBtn.addEventListener('click', () => {
+// closeNavBtn.addEventListener('click', () => {
+//     closeNavBtn.style.display = 'none';
+//     navItems.style.display = 'none';
+//     openNavBtn.style.display = 'inline-block';
+// })
+
+const closeNav = () => {
     closeNavBtn.style.display = 'none';
     navItems.style.display = 'none';
     openNavBtn.style.display = 'inline-block';
+}
+
+closeNavBtn.addEventListener('click', closeNav)
+
+
+// close nav menu when menu a menu item is clicked
+if (window.innerWidth < 1024){
+  document.querySelectorAll('#nav__items li a').forEach(navItem => {
+    navItem.addEventListener('click', () => {
+      closeNav();
+    })
+  })
+}
+
+
+//  change navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+  document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
+
+
+
+
+
+
 
 // TETIMONIALS SECTION (SWIPER JS)
 var swiper = new Swiper(".mySwiper", {
